b874aa2ea53270a6492f1ee9226b5a7988190729
ben-heil <ben.jer.heil@gmail.com>
2019-03-25 09:58:41 -0400

    1. Fixed issue with incorrectly checking whether curl and git were installed 2. Ensured that flake8 was being installed to conda

--- setup.sh
+++ setup.sh
@@ -1,18 +1,27 @@
-# Check that the dependancies you need are installed
-if [ -x "$(command -v curl)"]
+# Check that the dependencies you need are installed
+if [ ! -x "$(command -v curl)" ]
 then
 	echo "Error, curl is not installed" >&2
 	exit 1
 fi 
-if [ -x "$(command -v git)"]
+if [ ! -x "$(command -v git)" ]
 then
 	echo "Error, git is not installed" >&2
 	exit 1
 fi
 
 # Install miniconda 3, a python environment manager
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-bash Miniconda3-latest-Linux-x86_64.sh -b
+if [ ! -f ./Miniconda3-latest-Linux-x86_64.sh ]
+then
+	wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+fi
+
+bash Miniconda3-latest-Linux-x86_64.sh -b -p /home/$USER/miniconda
+
+# Add miniconda to the PATH in your bashrc
+echo -e '\nPATH=/home/$USER/miniconda/bin:$PATH' >> ~/.bashrc
+
+source ~/.bashrc
 
 # Install pathogen (a vim plugin manager)
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
@@ -24,4 +33,4 @@ cd ~/.vim/bundle && \
 	git clone --depth=1 https://github.com/vim-syntastic/syntastic.git
 
 # install flake8 (a python syntax checker used by syntastic)
-pip install flake8
+conda install -y flake8
