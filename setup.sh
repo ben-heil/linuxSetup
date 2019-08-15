8e13d556f853e823315ae29bf458cc26d815d4b1
Ben Heil <ben.jer.heil@gmail.com>
2019-08-15 15:45:08 -0400

    Streamlined setup.sh by moving vim plugin management to .vimrc

--- setup.sh
+++ setup.sh
@@ -1,14 +1,8 @@
-# Check that the dependencies you need are installed
-if [ ! -x "$(command -v curl)" ]
+if [ ! -x "$(command -v wget)" ]
 then
-	echo "Error, curl is not installed" >&2
+	echo "Error, wget is not installed" >&2
 	exit 1
 fi 
-if [ ! -x "$(command -v git)" ]
-then
-	echo "Error, git is not installed" >&2
-	exit 1
-fi
 
 # Install miniconda 3, a python environment manager
 if [ ! -f ./Miniconda3-latest-Linux-x86_64.sh ]
@@ -16,21 +10,7 @@ then
 	wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 fi
 
-bash Miniconda3-latest-Linux-x86_64.sh -b -p /home/$USER/miniconda
+bash Miniconda3-latest-Linux-x86_64.sh -b -p $HOME/miniconda
 
 # Add miniconda to the PATH in your bashrc
 echo -e '\nPATH=/home/$USER/miniconda/bin:$PATH' >> ~/.bashrc
-
-source ~/.bashrc
-
-# Install pathogen (a vim plugin manager)
-mkdir -p ~/.vim/autoload ~/.vim/bundle && \
-curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-
-# Install syntastic (a vim syntax checking plugin)
-
-cd ~/.vim/bundle && \
-	git clone --depth=1 https://github.com/vim-syntastic/syntastic.git
-
-# install flake8 (a python syntax checker used by syntastic)
-conda install -y flake8
